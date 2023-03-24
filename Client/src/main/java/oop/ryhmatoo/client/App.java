effd8dd91c2294c086b66965fbf8e2cc1c5ed4f1
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2023-03-24 11:20:45 +0200

    Lisasin algse maven struktuuri.

---
+++ Client/src/main/java/oop/ryhmatoo/client/App.java
@@ -0,0 +1,13 @@
+package oop.ryhmatoo.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
